5fd5311fc05efc0d68719509f3fa93fc1d7fdcde
Fraser Cassidy <l00101759@student.lyit.ie>
2016-02-10 23:04:44 +0000

    Added some code to Accelerometer Functionality. Still inaccuracte

--- app/src/main/java/com/example/fraser/mysecondapp/SensorActivity.java
+++ app/src/main/java/com/example/fraser/mysecondapp/SensorActivity.java
@@ -19,7 +19,7 @@ import android.widget.TextView;
 
 public class SensorActivity extends AppCompatActivity implements SensorEventListener {
 
-    private TextView xText,yText,zText, detectShake, x1Text,y1Text,z1Text;
+    private TextView xText,yText,zText, detectShake, x1Text,y1Text,z1Text, movementText;
     private Sensor mySensor;
     private SensorManager SM;
 
@@ -31,7 +31,6 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
     private static final int SHAKE_THRESHOLD = 600;
     private float[] gravity = new float[3];
     private float[] linear_acceleration = new float[3];
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,24 +53,25 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
         x1Text = (TextView)findViewById(R.id.x1Text);
         y1Text = (TextView)findViewById(R.id.y1Text);
         z1Text = (TextView)findViewById(R.id.z1Text);
+        movementText = (TextView)findViewById(R.id.movement);
 
         //initialize vibration
         v = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
 
     }
-
+    /*
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
 
 
-        //xText.setText("X: "+ sensorEvent.values[0]);
-        //yText.setText("Y: "+ sensorEvent.values[1]);
-        //zText.setText("Z: "+ sensorEvent.values[2]);
+       /* xText.setText("X: "+ sensorEvent.values[0]);
+        yText.setText("Y: "+ sensorEvent.values[1]);
+        zText.setText("Z: "+ sensorEvent.values[2]);
 
         /////////////////////////////
         //if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER)
         //{
-          /*  float x = sensorEvent.values[0];
+            float x = sensorEvent.values[0];
             float y = sensorEvent.values[1];
             float z = sensorEvent.values[2];
 
@@ -99,11 +99,11 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
                 xText.setText("X: "+ last_x);
                 yText.setText("Y: "+ last_y);
                 zText.setText("Z: "+ last_z);
-            }
-        //}*/
+            }*/
+        //}
         ///////////////////////////////////////
 
-       final float alpha = (float) 0.8;
+       /*final float alpha = (float) 0.8;
        // Isolate the force of gravity with the low-pass filter.
         gravity[0] = alpha * gravity[0] + (1 - alpha) * sensorEvent.values[0];
         gravity[1] = alpha * gravity[1] + (1 - alpha) * sensorEvent.values[1];
@@ -119,6 +119,14 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
         float y1 = sensorEvent.values[1] - gravity[1];
         float z1 = sensorEvent.values[2] - gravity[2];
 
+        if(x1 > 1)
+        {
+            movementText.setText("Movement: Up");
+        }
+        else
+            movementText.setText("Movement: ");
+
+
         last_x = x1;
         last_y = y1;
         last_z = z1;
@@ -127,6 +135,62 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
         x1Text.setText("X: "+ last_x);
         y1Text.setText("Y: "+ last_y);
         z1Text.setText("Z: "+ last_z);
+
+    }*/
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+        final float alpha = (float) 0.8;
+        // Isolate the force of gravity with the low-pass filter.
+        gravity[0] = alpha * gravity[0] + (1 - alpha) * sensorEvent.values[0];
+        gravity[1] = alpha * gravity[1] + (1 - alpha) * sensorEvent.values[1];
+        gravity[2] = alpha * gravity[2] + (1 - alpha) * sensorEvent.values[2];
+
+        // Remove the gravity contribution with the high-pass filter.
+        linear_acceleration[0] = sensorEvent.values[0] - gravity[0];
+        linear_acceleration[1] = sensorEvent.values[1] - gravity[1];
+        linear_acceleration[2] = sensorEvent.values[2] - gravity[2];
+
+        // acceleration
+        float x = sensorEvent.values[0] - linear_acceleration[0];
+        float y = sensorEvent.values[1] - linear_acceleration[1];
+        float z = sensorEvent.values[2] - linear_acceleration[2];
+
+        //float x = event.values[0];
+        //float y = event.values[1];
+        //float z = event.values[2];
+
+        x = Math.abs(x);
+        y = Math.abs(y);
+        z = Math.abs(z);
+
+        if (x > y)
+        {
+            if (x < 0) {
+                x1Text.setText("Right");//switch these values??
+            }
+            if (x > 0) {
+                x1Text.setText("Left");//with this one
+            }
+
+        }
+        else
+        {
+            if (y < 0)
+            {
+                x1Text.setText("Up");
+            }
+            if (y > 0)
+            {
+                x1Text.setText("Down");
+            }
+        }
+
+        if (x > (-2) && x < (2) && y > (-2) && y < (2)) {
+
+            x1Text.setText("Middle");
+
+        }
 
     }
 
